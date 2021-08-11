7ec5646f6ebf2acb845c0ee7e9af0bd07f3e4835
spwx <spwx@Seans-MacBook-Air.local>
2021-08-10 21:43:30 -0400

    Set FZF appearance in fish config

--- fish/config.fish
+++ fish/config.fish
@@ -16,4 +16,6 @@ if status --is-interactive
   set -gx LC_ALL en_US.UTF-8
   set -gx LANG en_US.UTF-8
   set -gx LANGUAGE en_US.UTF-8
+
+  set -g FZF_DEFAULT_OPTS "--layout=reverse --cycle --height=40% --color fg:#ebdbb2,bg:#282828,hl:#fabd2f,fg+:#ebdbb2,bg+:#3c3836,hl+:#fabd2f --color info:#83a598,prompt:#bdae93,spinner:#fabd2f,pointer:#83a598,marker:#fe8019,header:#665c54"
 end
