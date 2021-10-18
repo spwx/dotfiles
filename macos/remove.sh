9dd15d539404f00a27bc323e0b730aa2e944afbc
spwx <spwx@Seans-MacBook-Air.local>
2021-10-18 14:21:39 -0400

    Cleanup MacOS settings

---
+++ macos/remove.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+unlink ~/.config/yabai/yabairc
+unlink ~/.config/skhd/skhdrc
+unlink ~/.config/limelight/limelightrc
+unlink ~/.config/karabiner/karabiner.json
---
+++ macos/setup.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+./karabiner/setup.sh
+./limelight/setup.sh
+./skhd/setup.sh
+./yabai/setup.sh
