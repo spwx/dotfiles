a964ab00de97896418522c4cd57a9944d72621cb
spwx <spwx@Seans-MacBook-Air.local>
2021-10-18 14:37:28 -0400

    Cleanup Doom setup directories

--- doom/setup.sh
+++ doom/setup.sh
@@ -1,12 +1,4 @@
 #!/usr/bin/env bash
-set -euo pipefail
 
-# get the full path of this file
-SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
-
-rm -rf ~/.doom.d
-
-printf "Linking configuration directory...\n"
-ln -s "$SCRIPTPATH/" "$HOME/.doom.d"
-
-printf "Done!\n"
+./aspell/setup.sh
+./doom.d/setup.sh
