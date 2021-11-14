4111c48afe4d33dcf323c6aab468d32aaa2fbb17
spwx <spwx@spwxs-MacBook-Air.local>
2021-11-14 12:42:56 -0500

    Find a better way to remove the greeting

--- fish/config.fish
+++ fish/config.fish
@@ -19,5 +19,7 @@ if status --is-interactive
 
   set -g FZF_DEFAULT_OPTS "--layout=reverse --cycle --height=40% --color fg:#ebdbb2,bg:#282828,hl:#fabd2f,fg+:#ebdbb2,bg+:#3c3836,hl+:#fabd2f --color info:#83a598,prompt:#bdae93,spinner:#fabd2f,pointer:#83a598,marker:#fe8019,header:#665c54"
 
+  set fish_greeting
+
   source $HOME/.emacs.d/.local/straight/repos/emacs-libvterm/etc/emacs-vterm.fish
 end
