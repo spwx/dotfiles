a0fbde452dd05c4af6a6798ba3f4ffa6bf51dc7b
spwx <spwx@spwxs-MacBook-Air.local>
2021-11-14 12:43:33 -0500

    Get the latest and greatest rustic!

--- doom/doom.d/packages.el
+++ doom/doom.d/packages.el
@@ -48,6 +48,7 @@
 ;; Doom's packages are pinned to a specific commit and updated from release to
 ;; release. The `unpin!' macro allows you to unpin single packages...
 ;(unpin! pinned-package)
+(unpin! rustic)
 ;; ...or multiple packages
 ;(unpin! pinned-package another-pinned-package)
 ;; ...Or *all* packages (NOT RECOMMENDED; will likely break things)
