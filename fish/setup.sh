c06dba51b4aea408e4319bab32a2abd62750905a
spwx <spwx@spwxs-MacBook-Air.local>
2021-12-17 23:00:23 -0500

    Track fish_variables as well
    to manually set up the color scheme (base16 eighties)

--- fish/setup.sh
+++ fish/setup.sh
@@ -7,7 +7,8 @@ SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 printf "Creating ~/.config directory\n"
 mkdir -p ~/.config/fish
 
-printf "Linking configuration file...\n"
+printf "Linking configuration files...\n"
 ln -s "$SCRIPTPATH/config.fish" "$HOME/.config/fish/config.fish"
+ln -s "$SCRIPTPATH/fish_variables" "$HOME/.config/fish/fish_variables"
 
 printf "Done!\n"
