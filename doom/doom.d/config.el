1c26021312d57d81113fe483265e86ed55c1e626
spwx <spwx@spwxs-MacBook-Air.local>
2022-01-17 23:07:38 -0500

    Yes! Bring back vterm modelines!

--- doom/doom.d/config.el
+++ doom/doom.d/config.el
@@ -145,3 +145,5 @@
   (org-roam-dailies-capture-templates
    '(("d" "default" entry "* %<%I:%M %p>: %?"
       :if-new (file+head "%<%Y-%m-%d>.org" "#+title: %<%Y-%m-%d>\n")))))
+
+(remove-hook! 'vterm-mode-hook 'hide-mode-line-mode)
