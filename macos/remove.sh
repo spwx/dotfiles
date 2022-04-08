e23cb8efecd1222f82f6e56b0332bc1e138e8cb6
spwx <spwx@spwxs-MacBook-Air.local>
2022-04-08 18:44:06 -0400

    Updated active window indicator

--- macos/remove.sh
+++ macos/remove.sh
@@ -2,5 +2,4 @@
 
 unlink ~/.config/yabai/yabairc
 unlink ~/.config/skhd/skhdrc
-unlink ~/.config/limelight/limelightrc
 unlink ~/.config/karabiner/karabiner.json
--- macos/setup.sh
+++ macos/setup.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
 ./karabiner/setup.sh
-./limelight/setup.sh
 ./skhd/setup.sh
 ./yabai/setup.sh
