130830c1335e85dabcc39af9d5d22f2f5e84e1f6
spwx <spwx@spwxs-MacBook-Air.local>
2022-06-04 10:27:32 -0400

    Remove unnecessary key kinds
    Ctrl+j is back in doom emacs

--- doom/doom.d/config.el
+++ doom/doom.d/config.el
@@ -54,10 +54,6 @@
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
 
-(map! :leader
-      :desc "line below" "i o" #'+evil/insert-newline-below
-      :desc "line above" "i O" #'+evil/insert-newline-above)
-
 (map! :desc "parent headline"
       :map org-mode-map
       :n
