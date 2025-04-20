dd46d566a3e6fefdafce4fe22fe9b4b0c5f7e654
Sean <sean@wall.ninja>
2025-04-20 19:09:17 -0400

    feat: remove doom emacs

--- doom/aspell/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# get the full path of this file
-SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
-
-printf "Linking dictionary... "
-ln -s "$SCRIPTPATH/.aspell.en.pws" "$HOME/.aspell.en.pws"
-
-printf "Done!\n"
--- doom/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-# get the full path of this file
-SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
-
-# printf "Linking configuration directory...\n"
-$SCRIPTPATH/aspell/setup.sh
-$SCRIPTPATH/doom.d/setup.sh
