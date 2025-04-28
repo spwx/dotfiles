324cdd234f265be1bdac042c56691dbe2eede467
Sean Wall <spw@amazon.com>
2025-04-28 11:00:25 -0400

    fix(vscode-nvim): properly handle pre-existing config

--- vscode-neovim-config/setup.sh
+++ vscode-neovim-config/setup.sh
@@ -2,7 +2,19 @@
 set -euo pipefail
 
 # get the full path of this file
-SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+SCRIPTPATH="$(
+  cd "$(dirname "$0")"
+  pwd -P
+)"
+
+# Remove any older configuration
+printf "Removing any older configuration.\n"
+target="$HOME/.config/vscode-neovim-config"
+if [ -L "$target" ]; then
+  unlink "$target"
+else
+  rm -rf "$target"
+fi
 
 printf "Creating ~/.config directory\n"
 mkdir -p ~/.config/vscode-neovim-config
