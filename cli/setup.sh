dca8e83c0030afedf1dc8c01de801125ba0f2b84
Sean Wall <spw@amazon.com>
2025-05-11 21:09:55 -0400

    feat: restructure dotfiles

--- cli/setup.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
-$SCRIPT_DIR/fish/setup.sh
-$SCRIPT_DIR/starship/setup.sh
-$SCRIPT_DIR/tmux/setup.sh
