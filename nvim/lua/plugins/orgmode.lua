69f2497b0c22f9d559adf4208bc8096a6fd00ed7
Sean Wall <spw@amazon.com>
2025-05-22 15:59:42 -0400

    nit: add an org mode todo state

--- nvim/lua/plugins/orgmode.lua
+++ nvim/lua/plugins/orgmode.lua
@@ -41,10 +41,11 @@ return {
           org_open_at_point = "<CR>",
         },
       },
-      org_todo_keywords = { "TODO(t)", "PROG(p)", "WAIT(w)", "|", "DONE(d)" },
+      org_todo_keywords = { "TODO(t)", "PROG(p)", "WAIT(w)", "|", "DONE(d)", "STOP(s)" },
       org_todo_keyword_faces = {
         WAIT = ":foreground orange",
         PROG = ":foreground yellow",
+        STOP = ":foreground grey",
       },
     },
     init = function()
