4606bea2b7219333485622d2dd9998f7c58c5b23
Sean Wall <spw@amazon.com>
2025-08-25 12:04:22 -0400

    fix: one more thing

--- mac_setup.sh
+++ mac_setup.sh
@@ -2,6 +2,7 @@
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
 $SCRIPT_DIR/fish/setup.sh
+$SCRIPT_DIR/atuin/setup.sh
 $SCRIPT_DIR/starship/setup.sh
 $SCRIPT_DIR/tmux/setup.sh
 $SCRIPT_DIR/aerospace/setup.sh
